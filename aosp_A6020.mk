f2149a06de360927a61c335022fd8fea7468fab1
Goksigun <goksigun@protonmai.com>
2022-04-08 17:51:29 +0300

    A6020: Prepare for Pixel Experience

--- aosp_A6020.mk
+++ aosp_A6020.mk
@@ -21,20 +21,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk
 # Inherit from A6020 device
 $(call inherit-product, device/lenovo/A6020/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product-if-exists, vendor/arrow/config/common.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+IS_PHONE := true
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
-TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_WIDTH := 1080
 
 ## Device identifier. This must come after all inclusions
-DEVICE_MAINTAINER := Adithya R
 PRODUCT_DEVICE := A6020
-PRODUCT_NAME := arrow_A6020
+PRODUCT_NAME := aosp_A6020
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Vibe K5
 PRODUCT_MANUFACTURER := Lenovo
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="A6020"
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
